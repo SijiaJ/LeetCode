9870746707fe009a546f0d671c02b4ad12ccc095
jisj <824431986@qq.com>
2020-03-07 21:05:40 +0800

    动态规划，零钱问题

---
+++ java/CoinChange.java
@@ -0,0 +1,43 @@
+package dayN;
+/**
+ * 给定不同面额的硬币 coins 和一个总金额 amount。
+ * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+ *
+ * 示例 1:
+ *
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * 示例 2:
+ *
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ **/
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int [amount+1];
+        if(amount<0)return -1;
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            int cost = amount+1;
+            for (int coin:coins) {
+                if(i-coin<0)continue;
+                cost= Math.min(cost, dp[i-coin]+1);
+
+                System.out.println(cost);
+            }
+            dp[i]=cost;
+
+        }
+        return dp[amount]==amount+1?-1:dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange c = new CoinChange();
+        int[] coins= {1,2,5};
+        int amount = 11;
+        int result= c.coinChange(coins,amount);
+        System.out.println(result);
+    }
+}
